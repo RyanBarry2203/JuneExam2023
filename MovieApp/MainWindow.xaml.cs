c4ed1de42ff48f6a71e00043fe6540db47227577
S00250496 <barryryan12345@gmail.com>
2026-05-21 09:32:51 +0100

    paper more or less done

--- MovieApp/MainWindow.xaml.cs
+++ MovieApp/MainWindow.xaml.cs
@@ -22,6 +22,8 @@ namespace MovieApp
     public partial class MainWindow : Window
     {
         MovieData db;
+        private int availableSeats = 100;
+        private int availableSeatsForSelectedMovie = 100;
 
         public MainWindow()
         {
@@ -37,6 +39,8 @@ namespace MovieApp
                 var movies = db.Movies.ToList();
                 lbxMovies.ItemsSource = movies;
             }
+
+            txtAvailableSeats.Text = availableSeats.ToString();
         }
 
         private void lbxMovies_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -48,6 +52,43 @@ namespace MovieApp
             {
                 txtSynopsis.Text = $"Description: {selectedMovie.Description}\nCast: {selectedMovie.Cast}";
             }
+        }
+
+        private void btnBook_Click(object sender, RoutedEventArgs e)
+        {
+            // get selected movie, date and required seats from screen, create a new booking and save to database
+
+            db = new MovieData();
+
+            var selectedMovie = lbxMovies.SelectedItem as Models.Movie;
+            var selectedDate = dtpQuickBook.SelectedDate;
+            var requiredSeats = int.Parse(txtRequiredSeats.Text);
+
+            if (selectedMovie != null && selectedDate != null && requiredSeats > 0 && requiredSeats <= availableSeatsForSelectedMovie)
+            {
+                var booking = new Models.Booking
+                {
+                    MovieID = selectedMovie.MovieID,
+                    BookingDate = selectedDate.Value,
+                    NumberOfTickets = requiredSeats
+                };
+
+                using (db)
+                {
+                    db.Bookings.Add(booking);
+                    db.SaveChanges();
+                }
+
+                MessageBox.Show("Booking successful!");
+
+                // update available seats
+                availableSeats -= requiredSeats;
+                txtAvailableSeats.Text = availableSeats.ToString();
             }
+            else
+            {
+                MessageBox.Show("Sold Out!");
+            }
+        }
     }
 }
